5f6ba80fee1c56bec49351f97001d75a8a25acc5
FAROOQ KHAN <farooq.khan@caci.com>
2024-07-22 16:20:45 -0400

    Fetched data from DB so workouts in DB will be displayed

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react"
+import WorkoutDetails from "../components/WorkoutDetails"
+
 const Home = () => {
+	// state for workouts in DB
+	const [workouts, setWorkouts] = useState(null)
+
+	useEffect(() => {
+		// method to fetch workouts in DB
+		const fetchWorkouts = async () => {
+			const response = await fetch("/api/workouts")
+			const json = await response.json()
+
+			if (response.ok) {
+				setWorkouts(json)
+			}
+		}
+
+		fetchWorkouts()
+	}, [])
+
 	return (
 		<div className="home">
-			<h2>Home</h2>
+			<div className="workouts">
+				{workouts &&
+					workouts.map((workout) => (
+						<WorkoutDetails key={workout._id} workout={workout} />
+					))}
+			</div>
 		</div>
 	)
 }
